583b11bb3ebb938859d7c7c834fe9ebbb2aed5f8
Nick Avgerinos <taro@axcella.com>
2014-02-14 19:11:40 -0800

    Adding extra descriptions and test cases for ex9

--- Exercise09/recursion.py
+++ Exercise09/recursion.py
@@ -1,4 +1,28 @@
-# Multiply all the elements in a list
+"""
+Exercise 9: Recursion
+---------------------
+
+The Evil Wizard Malloc has attacked you and taken away your ability
+to use loops!  The only way you can defeat him is by fighting back
+using the power of recursion!
+
+Below are function stubs for a variety of functions that are able
+to be solved recursively.  For this exercise, you are not allowed
+to use:
+
+  * for loops
+  * "while" loops
+  * You may use list slicing, but you may not use the "reverse" slice
+    (e.g, list[::-1])
+  * Any built-in function that achieves the same result
+
+You can use the included test_recursion.py script to test that your
+functions work (and if you need hints for what the expected input/output 
+should be).
+
+"""
+
+# Multiply all the elements in a list using recursion
 def multiply_list(l):
     return 1
 
@@ -6,29 +30,32 @@ def multiply_list(l):
 def factorial(n):
     return 1
 
-# Count the number of elements in the list l
+# Count the number of elements in the list l without using loops or the len() function
 def count_list(l):
-    return
+    return 0
 
-# Sum all of the elements in a list
+# Sum all of the elements in a list without using loops or the sum() function
 def sum_list(l):
     return 0
 
-
-# Reverse a list without slicing or loops
+# Reverse a list recursively without loops, the reverse() function or list[::-1]
 def reverse(l):
     return []
 
 # Fibonacci returns the nth fibonacci number. The nth fibonacci number is
 # defined as fib(n) = fib(n-1) + fib(n-2)
 def fibonacci(n):
-    pass
+    return 1
 
 # Finds the item i in the list l.... RECURSIVELY
+# Return the item if it is in the list or None if not found.
 def find(l, i):
     return None
 
 # Determines if a string is a palindrome
+# A palindrome is any string that is the same forwards and backwords.
+#   (e.g. radar, racecar, aibohphobia)
+# Solve recursively, 
 def palindrome(some_string):
     return False
 
@@ -37,6 +64,17 @@ def fold_paper(width, height, folds):
     return (0, 0)
 
 # Count up
-# Print all the numbers from 0 to target
+# Print all the numbers from 0 to target.
+# Use n to keep track of where you're at
+# ex, to count from 1 - 100, call count_up(100, 1)
+#
+# Note: we don't have a test case for this one, so just run this
+#       script directly
+#
+# Note #2: We're printing out the numbers, so this script does not 
+#          need to return anything!
 def count_up(target, n):
-    return
+    pass
+
+if __name__ == "__main__":
+    count_up(100, 0)
---
+++ Exercise09/test_recursion.py
@@ -0,0 +1,78 @@
+import unittest
+import math
+
+from recursion import *
+
+class TestRecursionOperations(unittest.TestCase):
+
+    def setUp(self):
+        self.numbers1 = [5, 2, 1, 4, 3, 6]
+        self.numbers2 = [5, 24, 48, 88]
+        self.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
+                       'Sep', 'Oct', 'Nov', 'Dec']
+
+
+    ### Tests for recursion.py ###
+    def test_multiply_list(self):
+        self.assertEqual(multiply_list(self.numbers1), 720)
+        self.assertEqual(multiply_list(self.numbers2), 506880)
+
+    def test_factorial(self):
+        self.assertEqual(factorial(1), math.factorial(1))
+        self.assertEqual(factorial(2), math.factorial(2))
+        self.assertEqual(factorial(3), math.factorial(3))
+        self.assertEqual(factorial(4), math.factorial(4))
+        self.assertEqual(factorial(5), math.factorial(5))
+        self.assertEqual(factorial(10), math.factorial(10))
+        self.assertEqual(factorial(20), math.factorial(20))
+
+    def test_count_list(self):
+        self.assertEqual(count_list(self.numbers1), len(self.numbers1))
+        self.assertEqual(count_list(self.numbers2), len(self.numbers2))
+        self.assertEqual(count_list(self.months),   len(self.months))
+
+    def test_sum_list(self):
+        self.assertEqual(sum_list(self.numbers1), sum(self.numbers1))
+        self.assertEqual(sum_list(self.numbers2), sum(self.numbers2))
+
+    def test_reverse_list(self):
+        self.assertEqual(reverse(self.numbers1), self.numbers1[-1])
+        self.assertEqual(reverse(self.numbers2), self.numbers2[-1])
+        self.assertEqual(reverse(self.months), self.months[-1])
+
+    def test_fibonacci(self):
+        self.assertEqual(fibonacci(1), 1)
+        self.assertEqual(fibonacci(2), 1)
+        self.assertEqual(fibonacci(3), 2)
+        self.assertEqual(fibonacci(4), 3)
+        self.assertEqual(fibonacci(5), 5)
+        self.assertEqual(fibonacci(6), 8)
+        self.assertEqual(fibonacci(7), 13)
+        self.assertEqual(fibonacci(8), 21)
+        self.assertEqual(fibonacci(9), 34)
+
+    def test_find(self):
+        self.assertEqual(find(self.numbers1, 4), 4)
+        self.assertEqual(find(self.numbers1, 5), 5)
+        self.assertEqual(find(self.months, "Jan"), "Jan")
+        self.assertEqual(find(self.months, "Feb"), "Feb")
+        self.assertEqual(find(self.months, "Dec"), "Dec")
+        self.assertEqual(find(self.months, "Jul"), "Jul")
+        self.assertEqual(find(self.months, "Hex"), None)
+
+    def test_palindrome(self):
+        self.assertTrue(palindrome("radar"))
+        self.assertTrue(palindrome("racecar"))
+        self.assertTrue(palindrome("poop"))
+        self.assertFalse(palindrome("palindrome"))
+        self.assertFalse(palindrome("abcdecba"))
+
+    def test_fold_paper(self):
+        self.assertEqual(fold_paper(10, 10, 1), (10/2, 10/2))
+        self.assertEqual(fold_paper(10, 10, 2), (10/2/2, 10/2/2))
+        self.assertEqual(fold_paper(10, 10, 3), (10/2/2/2, 10/2/2/2))
+        self.assertEqual(fold_paper(10, 10, 4), (10/2/2/2/2, 10/2/2/2/2))
+        self.assertEqual(fold_paper(10, 10, 5), (10/2/2/2/2/2, 10/2/2/2/2/2))
+
+if __name__ == '__main__':
+    unittest.main()
