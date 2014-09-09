b0b1fb4ca37d3925db8bc37e4431f4057caf0539
Katie Lefevre <ktel1218@gmail.com>
2014-09-09 11:40:33 -0700

    Fix ambiguous instructions

--- skills1/skills1.py
+++ skills1/skills1.py
@@ -1,11 +1,14 @@
 # Things you should be able to do.
 
-# Write a function that takes a list and returns a new list with only the odd numbers.
-def all_odd(some_list):
+number_list = [-5, 6, 4, 8, 15, 16, 23, 42, 2, 7]
+word_list = [ "What", "about", "the", "Spam", "sausage", "spam", "spam", "bacon", "spam", "tomato", "and", "spam"]
+
+# Write a function that takes a list of numbers and returns a new list with only the odd numbers.
+def all_odd(number_list):
     return []
 
-# Write a function that takes a list and returns a new list with only the even numbers.
-def all_even(some_list):
+# Write a function that takes a list of numbers and returns a new list with only the even numbers.
+def all_even(number_list):
     return []
 
 # Write a function that takes a list of strings and a new list with all strings of length 4 or greater.
@@ -13,15 +16,15 @@ def long_words(word_list):
     return []
 
 # Write a function that finds the smallest element in a list of integers and returns it.
-def smallest(some_list):
+def smallest(number_list):
     return None
 
 # Write a function that finds the largest element in a list of integers and returns it.
-def largest(some_list):
+def largest(number_list):
     return None
 
 # Write a function that takes a list of numbers and returns a new list of all those numbers divided by two.
-def halvesies(some_list):
+def halvesies(number_list):
     return []
 
 # Write a function that takes a list of words and returns a list of all the lengths of those words.
@@ -29,17 +32,17 @@ def word_lengths(word_list):
     return []
 
 # Write a function (using iteration) that sums all the numbers in a list.
-def sum_numbers(numbers):
+def sum_numbers(number_list):
     return 0
 
 # Write a function that multiplies all the numbers in a list together.
-def mult_numbers(numbers):
+def mult_numbers(number_list):
     return 0
 
 # Write a function that joins all the strings in a list together (without using the join method) and returns a single string.
-def join_strings(string_list):
+def join_strings(word_list):
     return ""
 
 # Write a function that takes a list of integers and returns the average (without using the avg method)
-def average(numbers):
+def average(number_list):
     return 0
